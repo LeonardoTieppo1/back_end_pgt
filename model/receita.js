91ef1fc42b553ff3a4d4745ba7c8e07465ef3f86
Leonardo Oliveira Tieppo <leonardo.tieppo2@hotmail.com>
2024-05-11 14:16:56 -0300

    alteração back-end

--- model/receita.js
+++ model/receita.js
@@ -1,10 +1,17 @@
 const mongoose= require('mongoose')
 
 const receitaSchema=mongoose.Schema({
+    nome:{type: String},
+    ingredientes:[{nome:{type:String},quantidade:{type:Number}}],
+    modo_preparo:{type:Array},
+    calorias:{type:Number},
+    porções:{type:Number},
+    tipo:{type:String},
+    nutrientes:[{nome:{type:String},quantidade:{type:Number}}]
 
-    nome_receita:{type: String}
 })
 
-const Receita = mongoose.model('receitas_teste',receitaSchema,'receitas_teste')
+const Receita = mongoose.model('receitas2',receitaSchema,'receitas2')
+
 
 module.exports= Receita;
